c91105f2656965f83cb1aca52147a4a9e1ca2720
Matthew Barrick <alexander@happybunnylabs.com>
2021-12-02 16:32:46 +0000

    day 2, part 1 complete

--- Main.hs
+++ Main.hs
@@ -1,10 +1,14 @@
+{-# LANGUAGE MultiWayIf #-}
+
 module Main where
 import System.Environment
 import System.Exit
+import Text.Printf
+import Data.List.Split
 
 -- Advent of Code 2021
 -- Day 2
---  part 1 solution: 
+--  part 1 solution: 1990000
 --  part 2 solution: 
 
 part_1_test = "day2/aoc_02_test_1.txt"
@@ -13,8 +17,32 @@ part_2_test = "day2/aoc_02_test_2.txt"
 part_1_input = "day2/aoc_02_part_1.txt"
 part_2_input = "day2/aoc_02_part_2.txt"
 
-part1 :: [String] -> Int
-part1 _ = 0
+data SubInstruction =
+    Forward Int |
+    Down Int|
+    Up Int |
+    Stop   deriving (Show) 
+
+instructionStringFoward = "forward"
+instructionStringDown = "down"
+instructionStringUp = "up"
+
+
+parseInstruction :: ([Char],Int) -> SubInstruction
+parseInstruction (ins, num) = if    | ins == instructionStringFoward -> (Forward num)
+                                    | ins == instructionStringDown -> (Down num)
+                                    | ins == instructionStringUp -> (Up num)
+
+
+part1 :: [SubInstruction] -> (Int,Int)
+part1 (x:xs) = executeIns x (0,0) xs
+    
+executeIns :: SubInstruction -> (Int,Int) -> [SubInstruction] -> (Int, Int)
+executeIns (Forward n) (x,y) (z:zs) = executeIns z (x+n,y) zs
+executeIns (Down n) (x,y) (z:zs) = executeIns z (x,y+n) zs
+executeIns (Up n) (x,y) (z:zs) = executeIns z (x,y-n) zs
+executeIns (Stop) end _ = end         
+
 
 part2 :: [String] -> Int
 part2 _ = 0
@@ -30,14 +58,23 @@ getStringVals path = do
                         contents <- readFile path
                         return  (lines contents)
 
+instructParts :: [Char] -> ([Char], Int)
+instructParts s = instruct_format (splitOn " " s)
+    where
+        instruct_format (x:y:_) = (x,(read::String->Int) y)
+
+
 
 
 main :: IO()
 main = do 
             vals1 <- getStringVals part_1_input
             vals2 <- getStringVals part_2_input
-            print ( vals1)
-            print (part1 vals1)
+            let program =  (map parseInstruction ( map instructParts vals1)) ++ [Stop]
+            let final_state_1 = (part1 program)
+            let answer_part_1 = (\(x,y) -> x*y) final_state_1
+            --print (answer_part_1)
+            printf "Part 1 \n     final location (%d,%d) -> %d\n" (fst final_state_1) (snd final_state_1) answer_part_1
             exitWith ExitSuccess
             print ( vals2)
             print (part2 vals2)
