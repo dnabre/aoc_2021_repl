b81c2b66feac9adae9b79069e4444aa0396828e1
Matthew Barrick <alexander@happybunnylabs.com>
2021-12-08 14:05:37 +0000

    day 7, both parts done
    lots of cleanup to do

--- Main.hs
+++ Main.hs
@@ -1,111 +1,132 @@
-{-# LANGUAGE MultiWayIf #-}
-
 module Main where
-import System.Environment
-import System.Exit
+
 import Text.Printf
 import Data.List.Split
 import Data.List
-import Data.Char
 
 -- Advent of Code 2021
--- Day 3
---  part 1 solution: 2003336
---  part 2 solution: 
+-- Day 7
+--  part 1 solution: 339321
+--  part 2 solution: 95476244
+part_1_test::[Char]
+part_1_test = "day7/aoc_07_test_1.txt"
 
-part_1_test = "day3/aoc_03_test_1.txt"
-part_2_test = "day3/aoc_03_test_2.txt"
+part_2_test::[Char]
+part_2_test = "day7/aoc_07_test_2.txt"
 
-part_1_input = "day3/aoc_03_part_1.txt"
-part_2_input = "day3/aoc_03_part_2.txt"
+part_1_input::[Char]
+part_1_input = "day7/aoc_07_part_1.txt"
 
---tally::Char -> [Char] -> Int -> Int
-tally _ [] acc = acc
-tally value (x:xs) acc = if (value == x)  then (tally value xs (acc+1)) else (tally value xs acc)
-
-gamma_rate_char:: [Char] -> Int
-gamma_rate_char xs = gamma_rate (map digitToInt xs) 
-
-gamma_rate:: [Int] -> Int
-gamma_rate xs = if (count1 >= count0) then 1 else 0
-    where
-        count1 = tally 1 xs 0
-        count0 = tally 0 xs  0 
-
-epsilon_rate_char :: [Char] -> Int
-epsilon_rate_char xs = epsilon_rate (map digitToInt xs)
-
-epsilon_rate:: [Int] -> Int
-epsilon_rate xs = if (count1 <= count0) then 1 else 0
-    where
-        count1 = tally 1 xs 0
-        count0 = tally 0 xs  0 
-
-bin2Dec :: [Int] -> Int
-bin2Dec = foldl' (\acc x -> acc * 2 + x) 0
-
-bitchar_to_bitlist :: [[Char]] -> [[Int]]
-bitchar_to_bitlist = map str2intlist
-    where
-        str2intlist::[Char] -> [Int]
-        str2intlist xs = map digitToInt xs
+part_2_input::[Char]
+part_2_input = "day7/aoc_07_part_2.txt"
 
 
-filter_bit_list xs value pos = (fbl xs value pos []) 
-    where
-        fbl [] _ _ acc = acc
-        fbl (x:xs) value pos acc = if ((x !! pos) == value) then (fbl xs value pos (x:acc)) else (fbl xs value pos acc)
-
-part1 ::[[Char]] -> (Int,Int,Int)
-part1 vals = (g,e,g*e)
-    where
-        tv = transpose vals
-        g_list = map gamma_rate_char tv
-        e_list = map epsilon_rate_char tv
-        g = bin2Dec g_list
-        e = bin2Dec e_list
-
-
-
+part1 x = undefined
 
 part2 x = undefined
 
-
-
-
-getIntVals :: FilePath -> IO [Int]
-getIntVals path = do 
-                    contents <- readFile path
-                    return (map (read::String->Int) (lines contents))
 
 getStringVals :: FilePath -> IO [String]
 getStringVals path = do 
                         contents <- readFile path
                         return  (lines contents)
+string2int::[Char]->Int
+string2int = r
+    where 
+        r:: [Char] -> Int
+        r = read
+
+rowString2list :: [Char] -> [Int]
+rowString2list xs = map string2int (filter (\x->x /= "") $ splitOn " " xs)
+
+fuel2Cost::Int -> Int -> Int
+fuel2Cost n t = div (x*x + x )2
+    where 
+        x = abs (n - t)
+fuelCost:: Int -> Int -> Int
+fuelCost n t = abs ( n - t )
+
+totalfuelCost:: [Int] -> Int -> Int
+totalfuelCost ls t = sum (map (fuelCost t) ls) 
+
+totalfuelCost2:: [Int] -> Int -> Int
+totalfuelCost2 ls t = sum (map (fuel2Cost t) ls)
+
+reverse_total_fuel_cost_2:: Int -> [Int] -> [Int] -> Int
+reverse_total_fuel_cost_2 f ls (x:[]) = x
+reverse_total_fuel_cost_2 f ls (x:xs) = if (f == (totalfuelCost2 ls f)) then x else reverse_total_fuel_cost_2 f ls xs
 
 
+average::[Int]->Int
+average ls = div (sum ls)  (length ls)
 
-fst3 (a,_,_) = a
-snd3 (_,b,_) = b
-thrd3 (_,_,c) =c
+getMiddle::[Int]->Int
+getMiddle [] = 0
+getMiddle xs = (a' + b') `div` 2
+    where a' = head $ drop a xs
+          b' = head $ drop b xs
+          a = (n `div` 2)
+          b = n' - 1
+          n' = n `div` 2
+          n = length xs
 
---main :: IO()
-main = do 
-            printf "Advent of Code 2021, Day 3:\n"
-            vals1 <- getStringVals part_1_input
+median::[Int]->Int
+median [] = 0
+median xs = result
+    where result = if (n `mod` 2 == 0)
+                    then getMiddle sorted
+                    else head $ drop a sorted
+          a = (n - 1) `div` 2
+          n = length xs
+          sorted = sort xs
+
+min_pair::([Int],[Int])->(Int,Int)
+min_pair ((x:xs),(y:ys)) = min_pair_h xs ys (x,y)
+    where
+        min_pair_h [] [] (mn, v) = (mn,v)
+        min_pair_h (x:xs) (y:ys) (mn,v) = if (x < mn) then min_pair_h xs ys (x,y) else min_pair_h xs ys  (mn,v) 
+        
+main :: IO()
+main = do
+            printf "Advent of Code 2021, Day 7:\n"
+            vals1 <- getStringVals part_1_test
             printf "    read %d lines of input\n" (length vals1)
-            vals2 <- getStringVals part_2_test
-            printf "    read %d lines of input\n" (length vals2)
-            
-          --  let (g,e,answer) = part1 vals1 
-
-          --  printf "    Part 1\n        gamma: %d,    epsilon: %d, solution= %d \n" g e answer
-            print "\n"
-            let tvals2 = transpose vals2
-            print vals2
-          
-          --  let program2 =  (map parseInstruction ( map instructParts vals2)) ++ [Stop]
-          --  let final_state_2 = (part2 program2)
-          --  let answer_part_2 = (\(x,y,_) -> x*y) final_state_2
-
-          --  printf "   Part 2 \n     final location (%d,%d,%d) -> %d\n" (fst3 final_state_2) (snd3 final_state_2) ( thrd3 final_state_2) answer_part_2
+            vals2 <- getStringVals part_2_input
+         --   printf "    read %d lines of input\n" (length vals2)
+            let nvals = (map string2int (splitOn "," (head vals1)))
+            print nvals
+           
+            print (totalfuelCost nvals 1)
+            print (totalfuelCost nvals 3)
+            print (totalfuelCost nvals 10)
+           
+            let nvals2 = (map string2int (splitOn "," (head vals2)))
+            print nvals2
+            let ls = nvals2
+            let min = minimum ls
+            let max = maximum ls
+            let med = median ls
+         
+            printf "\n   min   : %d \n   max   : %d\n" min max
+      
+            printf "   median: %d \n" med  
+            --mapM_ print    [-5..5]  
+            let fuels2 = map (\x-> (x,totalfuelCost2  ls x)) [min..max]
+            let f4 = map (totalfuelCost2 ls) [min..max]
+            let f5 = sort f4
+           -- mapM_ print fuels2
+            --  105 780 131 too high
+            print (take 5 f5)
+            let answer = head f5
+            print answer
+            let aa=  (reverse_total_fuel_cost_2 answer ls ls)
+            print aa
+            printf "\ntotal fuel cost for target %d is %d (%d)\n" aa (totalfuelCost2 ls aa) answer
+            let q = [q| q <- [min .. max], answer == (totalfuelCost2 ls q)]
+            print q
+        --    let answer1 = part1 state_1                    
+        --   printf "\n    Part 1\n         Solution: %d\n" answer1
+        --    let answer2 = part2 state_1
+        --    printf "    Part 2\n         Solution: %d\n" answer2
+            print "done"
+         
--- README.md
+++ README.md
@@ -7,8 +7,11 @@
 | [Day 2: Dive!](https://adventofcode.com/2021/day/2)  | Submarine Turtle | *   | * |
 | [Day 3: Binary Diagnostic](https://adventofcode.com/2021/day/3)  | Understanding a badly written spec | *   | _ |
 | [Day 4: Giant Squid](https://adventofcode.com/2021/day/4)  | Bingo | *   | * |
-| [Day 5: Hydrothermal Venture](https://adventofcode.com/2021/day/5)| Histogram  | _   | _ |
+| [Day 5: Hydrothermal Venture](https://adventofcode.com/2021/day/5)| Histogram  | *   | * |
 | [Day 6: Lanternfish](https://adventofcode.com/2021/day/6)  | Automata | *   | * |
+| [Day 7: The Treachery of Whales](https://adventofcode.com/2021/day/7)   | Maths | *   | * |
+| [Day 8: Seven Segment Search](https://adventofcode.com/2021/day/8)  | Crypto | _   | _ |
+| [](https://adventofcode.com/2021/day/10)  |  | _   | _ |
 |   |  | _   | _ |
 |   |  | _   | _ |
 |   |  | _   | _ |
