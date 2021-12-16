6cb76cd15496fb8fbb381fbc393a720b1134eef1
Matthew Barrick <alexander@happybunnylabs.com>
2021-12-16 22:10:52 +0000

    day3 extra clean

--- Day3.hs
+++ Day3.hs
@@ -1,11 +1,8 @@
-
-
 module Main where
 
 import Text.Printf
 import Data.List
 import Data.Char
-
 
 -- Advent of Code 2021
 -- Day 3
@@ -22,21 +19,8 @@ part_1_input = "day3/aoc_03_part_1.txt"
 part_2_input::[Char]
 part_2_input = "day3/aoc_03_part_2.txt"
 
-count1::[Int] -> Int
-count1 xs = if (ones >= zeros) then 1 else 0
-    where
-        ones = tally 1 xs 0
-        zeros = tally 0 xs 0
-
-count0::[Int] -> Int
-count0 xs = if (zeros <= ones) then 0 else 1
-    where
-        ones = tally 1 xs 0
-        zeros = tally 0 xs 0
-
-
 countOnes :: Int -> [[Char]] -> Int
-countOnes idx lines = length $ filter (== '1') $ map (!! idx) lines
+countOnes idx ls = length $ filter (== '1') $ map (!! idx) ls
 
 tally:: Int -> [Int] -> Int -> Int
 tally _ [] acc = acc
@@ -72,17 +56,14 @@ bitchar_to_bitlist = map str2intlist
 charBin2Dec::[Char]->Int
 charBin2Dec ss = bin2Dec (head (bitchar_to_bitlist [ss]))
 
-
-
-
-
-getRating n [x] _ = x
-getRating n lines bits = getRating (n+1)  next_lines bits
+getRating::Int->[[Char]]->(Char,Char)->[Char]
+getRating _ [x] _ = x
+getRating n ls bits = getRating (n+1)  next_lines bits
     where
         (ll,rr) = bits
-        ones = countOnes n lines
-        fewBits = if (2*ones) >= (length lines) then ll else rr
-        next_lines = filter (\s-> (s !! n) == fewBits ) lines
+        ones = countOnes n ls
+        fewBits = if (2*ones) >= (length ls) then ll else rr
+        next_lines = filter (\s-> (s !! n) == fewBits ) ls
 
 part1 ::[[Char]] -> (Int,Int,Int)
 part1 vals = (g,e,g*e)
@@ -93,41 +74,31 @@ part1 vals = (g,e,g*e)
         g = bin2Dec g_list
         e = bin2Dec e_list
 
-
-
-
-part2 lines = product (map charBin2Dec ratings)
+part2::[[Char]]->Int
+part2 ls = product (map charBin2Dec ratings)
     where
-        ratings = map (\b->getRating 0 lines b) [('1','0'),('0','1')]
-            
-getIntVals :: FilePath -> IO [Int]
-getIntVals path = do 
-                    contents <- readFile path
-                    return (map (read::String->Int) (lines contents))
+        ratings = map (\b->getRating 0 ls b) [('1','0'),('0','1')]         
 
 getStringVals::FilePath -> IO [[Char]] 
 getStringVals path = do 
                         contents <- readFile path
                         return  (lines contents)
-getOneCounts::[[Char]]->[Int]
-getOneCounts lines = map (\i-> countOnes i lines) [0..(binLength-1)]
-    where
-        binLength = length (head lines)
 
+main::IO ()
 main = do 
             printf "Advent of Code 2021, Day 3:\n"
             vals1 <- getStringVals part_1_input
             printf "    read %d lines of input\n" (length vals1)
-            lines <- getStringVals part_2_input
-            printf "    read %d lines of input\n" (length lines)
+            ls <- getStringVals part_2_input
+            printf "    read %d lines of input\n" (length ls)
             
             let (_,_,answer1) = part1 vals1                   
             printf "\n    Part 1\n         Solution: %d\n" answer1
           
-            let answer2 = part2 lines
+            let answer2 = part2 ls
 
             printf "    Part 2\n         Solution: %d\n" answer2
-            printf "\n\n    ---  done ---     \n"
+
 
 
 
