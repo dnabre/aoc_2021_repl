ce7886a7696cb4739e503b250490aac7287f113b
Matthew Barrick <alexander@happybunnylabs.com>
2021-12-24 14:06:01 +0000

    infrastructure compiles

--- Main.hs
+++ Main.hs
@@ -34,12 +34,14 @@ data Ins
 
 
 
+setState::Reg->Data->(Data,Data,Data,Data)->(Data,Data,Data,Data)
 setState W n (w,x,y,z) = (n,x,y,z)
 setState X n (w,x,y,z) = (w,n,y,z)
 setState Y n (w,x,y,z) = (w,x,n,z)
 setState Z n (w,x,y,z) = (w,x,y,n)
 
 
+getState::Reg->(Data,Data,Data,Data)->Data
 getState W (w,x,y,z) = w 
 getState X (w,x,y,z) = x
 getState Y (w,x,y,z) = y
@@ -71,13 +73,13 @@ getStringVals path = do
                         return  (lines contents)
 
 
-
+parseVar::[Char]->Reg
 parseVar "w" = W
 parseVar "x" = X
 parseVar "y" = Y
 parseVar "z" = Z
 
-
+parseParam::[Char]->Either Reg Data
 parseParam ps = if isDigit (head ps) then Right (string2int ps)  else Left (parseVar ps)
     where
         parseVar' "w" = Left W
@@ -85,7 +87,7 @@ parseParam ps = if isDigit (head ps) then Right (string2int ps)  else Left (pars
         parseVar' "y" = Left Y
         parseVar' "z" = Left Z
 
-
+parseLine::[[Char]]->Ins
 parseLine ["inp", var] = Inp (parseVar var)
 parseLine ss = i
     where        
@@ -99,7 +101,35 @@ parseLine ss = i
                 "mod" -> Ins Mod vs ps
                 "eql" -> Ins Eql vs ps
 
+doOp::Op->Data->Data->Data
+doOp Add r p = r+p
+doOp Mul r p = r*p
+doOp Div r p = if ( p==0) then error "divide by zero" else (div r p)
+doOp Mod r p = mod r p
+doOp Eql r p = if (r == p) then 1 else 0
 
+getParam::(Either Reg Data)->(Data,Data,Data,Data)->Data
+getParam (Right x)  state = x
+getParam (Left r)   state = getState r state
+
+initial_state::(Data,Data,Data,Data)
+initial_state = (-1,-1,-1,-1)
+
+
+{-
+run [] state _ = state
+run ins_list@(i:is) state input@(inp:ins) = run is nextState nextInput
+    where  
+        (rreg,pp) = case i of
+            Inp rx -> (rx,Right 0)
+            Ins _ rx p -> (rx,p)
+        rv = getState rreg state
+        p = getParam pp state 
+        oop = case i of 
+            Inp _ -> (setState rreg inp, ins)
+            Ins o _ _ -> (setState rreg (doOp o rv p), input) 
+        (nextState,nextInput) =  oop
+-}
 main :: IO()
 main = do 
             printf "Advent of Code 2021, Day 24:\n"
@@ -110,6 +140,9 @@ main = do
             print vals1
             let ins = map parseLine (map words vals1)
             print ins
+            let input = [1,3]
+  --          let result = run ins initial_state input
+
 {-      
             let answer1 = vals1
             printf "\n   Part 1    Solution: %d \n" answer1         
